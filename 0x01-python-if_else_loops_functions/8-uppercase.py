d84684b14b65343aeb4e0a79450f985751bd3aa7
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-10-03 06:00:40 -0700

    completed

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for c in str:
+        print("{:c}"
+              .format(ord(c) if ord(c) >= 65 and ord(c) <= 90 else ord(c)-32),
+              end="")
