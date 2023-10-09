ab24ec325e85e97a78204d3d86eecd6950d43377
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-10-09 13:35:57 -0700

    completed

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -3,7 +3,7 @@ def divisible_by_2(my_list=[]):
     if not my_list:
         return None
     new_list = []
-    for i int my_list:
+    for i in my_list:
         if i % 2 == 0:
             new_list.append(True)
         else:
