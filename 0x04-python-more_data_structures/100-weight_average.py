d3609b653b6b359316952236bd58d50532605097
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-10-11 13:22:40 -0700

    completed

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,5 @@
+def weight_average(my_list=[]):
+    if not my_list:
+        return 0
+    else:
+        return (sum(score * weight for score, weight in my_list) / sum(weight for score, weight in my_list))
