bc419fec0638ae2ff9c9ea13d8aa22649665e748
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-10-11 13:15:35 -0700

    completed

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,7 @@
 def roman_to_int(roman_string):
     if not isinstance(roman_string, str) :
         return 0
+
     roman_value = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     result = 0
     prev_value = 0
@@ -13,4 +14,3 @@ def roman_to_int(roman_string):
             result += current_value
         prev_value = current_value
     return result
-
