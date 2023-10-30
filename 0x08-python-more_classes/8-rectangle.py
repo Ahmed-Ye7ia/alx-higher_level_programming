210b4ae0e272eb25f6a394c3f9107a1597f725cd
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-10-30 05:48:51 -0700

    completed

---
+++ 0x08-python-more_classes/8-rectangle.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""
+Rectangle class
+"""
+
+
+class Rectangle:
+    """
+    class definition
+    """
+    number_of_instances = 0
+    print_symbol = '#'
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        string = "\n".join([str(self.print_symbol) * self.__width
+                            for j in range(self.__height)])
+        return string
+
+    def __repr__(self):
+        return "Rectangle({:d}, {:d})".format(self.width, self.height)
+
+    def __del__(self):
+        print("Bye rectangle...")
+        Rectangle.number_of_instances -= i
+
+    def bigger_or_equal(rect_1, rect_2):
+        if not isinstance((Rectangle),rect_1):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance((Rectangle),rect_2):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        return rect_2
