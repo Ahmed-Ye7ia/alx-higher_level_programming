d14db89e3aa23ba3f9528190038a7f1b77356512
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-10-30 05:42:32 -0700

    completed

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -43,7 +43,7 @@ class Rectangle:
     def __str__(self):
         if self.__width == 0 or self.__height == 0:
             return ""
-        string = "\n".join([str(Rectangle.print_symbol) * self.__width for j in range(self.__height)])
+        string = "\n".join([str(self.print_symbol) * self.__width for j in range(self.__height)])
         return string
 
     def __repr__(self):
