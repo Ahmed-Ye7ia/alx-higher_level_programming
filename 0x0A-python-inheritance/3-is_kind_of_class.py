82de5d9fcfaa197c105210e1a5e717140dbd9b36
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-11-06 05:50:40 -0800

    completed

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+'''Module for check if the object is an instance of a class.'''
+
+
+def is_kind_of_class(obj, a_class):
+    '''Determines if an object is instance of a class.'''
+    return isinstance(obj, a_class)
