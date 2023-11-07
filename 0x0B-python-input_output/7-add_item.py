2255492b7ca286c6a268578a4980e71fccbe78c1
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-11-07 08:07:19 -0800

    completed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,11 +4,11 @@ adds all arguments to a Python list, and then save them to a file:
 """
 
 
-from sys import argv
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-arglist = list(argv[1:])
+arglist = list(sys.argv[1:])
 
 try:
     old_content = load_from_json_file("add_item.json")
