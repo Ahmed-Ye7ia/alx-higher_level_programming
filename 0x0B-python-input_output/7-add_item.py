e3167c15b311595ae3b388779f51aed861fbc0e4
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-11-07 08:23:29 -0800

    completed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """
-This module defines a script which takes argvs
-and appends them to a python list, then writes
-the data in a new text file 'add_item.json' in
-JSON format.
+a script that adds all arguments to a Python list,
+and then save them to a file:
 """
 
 
@@ -11,11 +9,12 @@ from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-filename = "add_item.json"
+arglist = list(argv[1:])
 
 try:
-    existing_content = load_from_json_file(filename)
+    old_content = load_from_json_file("add_item.json")
 except FileNotFoundError:
-    existing_content = []
+    old_content = []
 
-save_to_json_file(existing_content + argv[1:], filename)
+old_content.extend(arglist)
+save_to_json_file(old_cotent, "add_item.json")
