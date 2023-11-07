5b9efac0ae8520d182889f942ca3d0fe8a0c833e
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-11-07 08:28:39 -0800

    completed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """
-a script that adds all arguments to a Python list,
-and then save them to a file:
+a script that adds all arguments to a Python list
 """
 
 
@@ -9,12 +8,12 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-arglist=list(sys.argv[1:])
+arglist = list(sys.argv[1:])
 
 try:
-    old_content=load_from_json_file("add_item.json")
-except FileNotFoundError:
+    old_content = load_from_json_file('add_item.json')
+except Exception:
     old_content=[]
 
 old_content.extend(arglist)
-save_to_json_file(old_cotent, "add_item.json")
+save_to_json_file(old_cotent, 'add_item.json')
