da1c694986746ec4631602bbc6337f3a7cd96e2d
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-11-07 08:31:18 -0800

    completed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,7 +10,4 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 arglist = list(sys.argv[1:])
 
-old_content = load_from_json_file('add_item.json')
-
-old_content.extend(arglist)
 save_to_json_file(arglist, 'add_item.json')
