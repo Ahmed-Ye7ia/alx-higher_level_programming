d7422cb38af55d368230b039dc430af7f77efe0a
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-11-07 08:24:47 -0800

    completed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,16 +5,16 @@ and then save them to a file:
 """
 
 
-from sys import argv
+import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-arglist = list(argv[1:])
+arglist=list(sys.argv[1:])
 
 try:
-    old_content = load_from_json_file("add_item.json")
+    old_content=load_from_json_file("add_item.json")
 except FileNotFoundError:
-    old_content = []
+    old_content=[]
 
 old_content.extend(arglist)
 save_to_json_file(old_cotent, "add_item.json")
