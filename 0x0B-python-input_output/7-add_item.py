d4e094d7561263b923be9b2524793aac8ec34e31
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-11-07 08:30:03 -0800

    completed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,10 +10,7 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 arglist = list(sys.argv[1:])
 
-try:
-    old_content = load_from_json_file('add_item.json')
-except Exception:
-    old_content=[]
+old_content = load_from_json_file('add_item.json')
 
 old_content.extend(arglist)
 save_to_json_file(old_cotent, 'add_item.json')
