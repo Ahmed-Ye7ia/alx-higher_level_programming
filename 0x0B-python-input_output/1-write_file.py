63007158e53784ff9b20189c7d8652fe799f8eb2
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2023-11-07 06:57:04 -0800

    completed

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -7,4 +7,4 @@ Contains function that writes a string to a text file
 def write_file(filename="", text=""):
     """writes to text file and retunrs num chars written"""
     with open(filename, 'w', encoding="utf-8") as f:
-        return(f.write(text))
+        return f.write(text)
