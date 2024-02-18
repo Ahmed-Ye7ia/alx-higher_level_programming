26a626f6caa6c14893e00c74ca93ba20b1e4927b
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2024-02-18 11:02:23 -0800

    completed

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+lists all states from the database hbtn_0e_0_usa
+Results must be sorted in ascending order by states.id
+"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+
+    db = MySQLdb.connect(host = "local host",
+            port = 3306,
+            user = argv[1],
+            passwd = argv[2],
+            db = argv[3])
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    for row in cursor.fetchall():
+        print(row)
+
+    curso.close()
+    db.close()
