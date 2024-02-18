e7dec0ca9c1ce592b45b76e2e364ec780f3aa9a6
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2024-02-18 11:09:21 -0800

    completed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -21,5 +21,5 @@ if __name__ == "__main__":
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
     for row in cursor.fetchall():
         print(row)
-    curso.close()
+    cursor.close()
     db.close()
