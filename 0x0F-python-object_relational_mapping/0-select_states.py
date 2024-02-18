09ff1fbf1c47ca48bfd603671f5b2659c64f6a32
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2024-02-18 12:34:49 -0800

    completed

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,20 +5,20 @@ Results must be sorted in ascending order by states.id
 """
 
 import MySQLdb
-from sys import argv
+import sys
 
 if __name__ == "__main__":
 
     # connect to database
     data = MySQLdb.connect(host = "local host",
-                         port = 3306,
-                         user = argv[1],
-                         passwd = argv[2],
-                         db = argv[3])
+                           port = 3306,
+                           user = sys.argv[1],
+                           passwd = sys.argv[2],
+                           db = sys.argv[3])
 
     # creatint cursor
     cursor = data.cursor()
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    cursor.execute("SELECT * FROM states")
     for row in cursor.fetchall():
         print(row)
     cursor.close()
