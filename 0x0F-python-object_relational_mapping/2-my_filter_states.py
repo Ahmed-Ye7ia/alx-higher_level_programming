286cbc5f3413a813310f2fa7ad9e1c32f2c35ba9
Ahmed-Ye7ia <ahmedye7ia03@gmail.com>
2024-02-23 04:46:16 -0800

    completed

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
-"""  lists all states from the database hbtn_0e_0_usa """
+"""
+return matching states
+parameters given to script: username, password, database, state to match
+"""
+
 import MySQLdb
 from sys import argv
 
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=argv[1],
-                         passwd=argv[2], db=argv[3], port=3306)
+
+    # connect to database
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+
+    # create cursor to exec queries using SQL; match arg given
     cursor = db.cursor()
     sql_cmd = """SELECT *
                  FROM states
-                 WHERE neme LIKE BINARY '{}'""".format(argv[4])
+                 WHERE name LIKE '{}' ORDER BY id ASC""".format(argv[4])
     cursor.execute(sql_cmd)
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
+    for row in cursor.fetchall():
+        if row[1] == argv[4]:
+            print(row)
     cursor.close()
     db.close()
